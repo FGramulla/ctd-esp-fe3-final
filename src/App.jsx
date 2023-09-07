d542b7c5a35de835cd337ae19e5d53eccaef13c8
maurotapiadev <maurotapiadev@gmail.com>
2023-09-07 20:41:19 -0300

    primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Detail from "./Routes/Detail";
+import Favs from "./Routes/Favs";
+import { Route, Routes } from 'react-router-dom';
+import Card from "./Components/Card";
+
 
 
 function App() {
   return (
       <div className="App">
+          <Home/>
+          {/* <Routes>
+            <Route path="/" element={Home}/>
+            <Route path="/Contact" element={Contact}/>
+            <Route path="/dentista/:id" element={Detail}/>
+            <Route path="/Favs" element={Favs}/>
+          </Routes> */}
           <Navbar/>
           <Footer/>
       </div>
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,30 @@
+import axios from "axios";
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Link } from 'react-router-dom'
+
 
 
 const Card = ({ name, username, id }) => {
+
+  const [doctor, setDoctores] = useState([]);
+
+  const url = "https://jsonplaceholder.typicode.com/users";
+
+  const fetchDoc = async () => {
+    try {
+        const response = await axios.get(url);
+        setDoctores(response.data);
+    } catch (error) {
+        console.log("error fetching doctor", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchDoc();
+  }, [])
+
 
   const addFav = ()=>{
     // Aqui iria la logica para agregar la Card en el localStorage
@@ -10,11 +33,20 @@ const Card = ({ name, username, id }) => {
   return (
     <div className="card">
         {/* En cada card deberan mostrar en name - username y el id */}
-
+        {doctor.map(doc => (
+        <div key={doc.id} className="card-doc">
+          
+          <img src="../img/doctor.jpg" height={150} alt=""/>
+          <p>{doc.name}</p>
+          <p>{doc.username}</p>
+          <p>{doc.id}</p>
+          <button onClick={addFav} className="favButton">Add fav</button>
+        </div>
+        
+        ))}
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
         {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        <button onClick={addFav} className="favButton">Add fav</button>
     </div>
   );
 };
